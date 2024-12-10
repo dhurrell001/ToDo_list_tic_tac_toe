b9aada14b192ee9cb37ab396d7d7916df91d209d
David Hurrell <dhurrell@hotmail.co.uk>
2024-12-10 18:16:35 +0000

    Added a selection of task lists. Add func to select a random list. Mapped selected list to tile

--- components/toDoGrid.js
+++ components/toDoGrid.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import ToDoTile from "./toDoTile";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GameLogic from "./GameLogic"; // Import GameLogic component
 
 export default function ToDoGrid({ setWinner }) {
@@ -17,6 +17,55 @@ export default function ToDoGrid({ setWinner }) {
     { id: "8", text: "No phone 1 hour", pressed: false },
     { id: "9", text: "Sing a song!", pressed: false },
   ]);
+
+  const todoTaskLists = [
+    [
+      { id: "1", text: "Washing up", pressed: false },
+      { id: "2", text: "Brush teeth twice", pressed: false },
+      { id: "3", text: "Fold clothes 20 min", pressed: false },
+      { id: "4", text: "Eat Breakfast", pressed: false },
+      { id: "5", text: "Exercise 20 mins", pressed: false },
+      { id: "6", text: "Watch a Movie", pressed: false },
+      { id: "7", text: "Relax 30 mins", pressed: false },
+      { id: "8", text: "No phone 1 hour", pressed: false },
+      { id: "9", text: "Sing a song!", pressed: false },
+    ],
+
+    [
+      { id: "1", text: "Washing up", pressed: false },
+      { id: "2", text: "Read 20 mins", pressed: false },
+      { id: "3", text: "Vacuum 1 room", pressed: false },
+      { id: "4", text: "Drink water!", pressed: false },
+      { id: "5", text: "Have shower", pressed: false },
+      { id: "6", text: "Brush teeth", pressed: false },
+      { id: "7", text: "No social media 1 hour", pressed: false },
+      { id: "8", text: "No phone 1 hour", pressed: false },
+      { id: "9", text: "Take a break", pressed: false },
+    ],
+    [
+      { id: "1", text: "Dance", pressed: false },
+      { id: "2", text: "Listen to music 20 mins", pressed: false },
+      { id: "3", text: "Wash hair", pressed: false },
+      { id: "4", text: "Drink water!", pressed: false },
+      { id: "5", text: "Relax 15 mins", pressed: false },
+      { id: "6", text: "Brush teeth", pressed: false },
+      { id: "7", text: "No TV 1 hour", pressed: false },
+      { id: "8", text: "Wash 1 load of laundry", pressed: false },
+      { id: "9", text: "Write a grocery list", pressed: false },
+    ],
+  ];
+
+  const [selectedTasks, setSelectedTasks] = useState([]);
+
+  useEffect(() => {
+    // Example: Randomly select a task list
+    //   const keys = Object.keys(taskLists);
+    const randomNumber = Math.floor(Math.random() * todoTaskLists.length);
+    console.log(`number ${randomNumber}`);
+    setSelectedTasks(
+      todoTaskLists[randomNumber].map((task) => ({ ...task, pressed: false }))
+    );
+  }, []);
   // Function to toggle the pressed state of a tile. This
   // function is passed to each tile on creation
   const togglePressed = (id) => {
@@ -30,7 +79,7 @@ export default function ToDoGrid({ setWinner }) {
   // a new tile, assign values accordingly.
   return (
     <View style={styles.container}>
-      {tiles.map((tile) => (
+      {selectedTasks.map((tile) => (
         <ToDoTile
           key={tile.id}
           id={tile.id}

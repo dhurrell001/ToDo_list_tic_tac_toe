bc8532ad59b2676b0366bc9a0150dbd07f7a3e29
David Hurrell <dhurrell@hotmail.co.uk>
2024-12-27 14:59:13 +0000

    Add reset button. Function to create new task list

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import ToDoGrid from "../components/toDoGrid";
 import Header from "../components/header";
 import WinnerText from "../components/winnerText";
@@ -8,19 +8,36 @@ import WinnerPage from "./WinnerPage";
 import { useNavigation } from "@react-navigation/native";
 
 export default function HomePage() {
-  const { winner, FullHouseWinner } = useContext(GameContext);
+  const {
+    winner,
+    FullHouseWinner,
+    todoTaskLists,
+    setSelectedTasks,
+    setFullhouseWinner,
+  } = useContext(GameContext);
   const navigation = useNavigation();
+
+  const createTaskList = () => {
+    const randomNumber = Math.floor(Math.random() * todoTaskLists.length);
+    const Tasklist = todoTaskLists[randomNumber].map((task) => ({
+      ...task,
+      pressed: false,
+    }));
+    setSelectedTasks(Tasklist);
+  };
   //Check if there is a 3 in a row winner or full house worker. Redirect to winnerpage
   //if winner found.
   useEffect(() => {
     if (winner || FullHouseWinner) {
       navigation.navigate("Winner"); // Navigate to WinnerPage
+      //   setFullhouseWinner(false);
     }
   }, [winner, FullHouseWinner, navigation]);
   return (
     <View style={styles.container}>
       <Header />
       <ToDoGrid />
+      <Button title="Reset" onPress={createTaskList} />
     </View>
   );
 }
@@ -28,7 +45,7 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "midnightblue",
+    backgroundColor: "rgb(21, 97, 109)",
     paddingLeft: 10,
     paddingRight: 10,
   },
